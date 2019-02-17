b8f1b4b75a6b82c34639f80cee13dea4464df6e1
Versawe <47580432+Versawe@users.noreply.github.com>
2019-02-16 21:28:11 -0500

    Boulder stacking
    The boulder don't stack anymore and they roll off each other

---
+++ ProjectBuild2/Assets/Scripts/Boulder_Pace.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boulder_Pace : MonoBehaviour
+{
+
+    private int Timer = 0;
+    private float pace_speed = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        Timer += 1;
+
+        if(Timer <= 30)
+        {
+            pos.z += pace_speed * Time.deltaTime;
+        }
+        if(Timer > 30)
+        {
+            pos.z += -pace_speed * Time.deltaTime;
+        }
+        if(Timer >= 60)
+        {
+            Timer = 0;
+        }
+
+        transform.position = pos;
+
+    }
+}
