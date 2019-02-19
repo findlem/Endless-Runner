91b5e8d1dab85b2320275a1e0d0b7faa4946df50
Versawe <47580432+Versawe@users.noreply.github.com>
2019-02-19 18:36:02 -0500

    aabb working

---
+++ ProjectBuild2/Assets/Scripts/CheckPickAxeAABB.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPickAxeAABB : MonoBehaviour
+{
+    ColliderAABB cb;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        cb = GetComponent<ColliderAABB>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Pickaxe_attack.Active == true)
+        {
+
+            cb.halfSize.z = 5f;
+            
+        }
+
+
+    }
+}
